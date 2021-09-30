65373471fec975d7f818f7e84ff936355fb97689
Zsolt Toth <tothzs87@gmail.com>
2021-09-30 10:52:58 +0200

    refactor(npm-example): add semi rule to eslint config

--- npm-example/index.js
+++ npm-example/index.js
@@ -1,5 +1,5 @@
-const winston = require('winston')
-const quadratic = require('./src/quadratic')
+const winston = require('winston');
+const quadratic = require('./src/quadratic');
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -9,17 +9,17 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: 'logs/combined.log' }),
     new winston.transports.Console()
   ]
-})
+});
 
 // console.log('Hello World!');
 setTimeout(() => {
-  logger.info('Hello World!')
-}, 5000)
+  logger.info('Hello World!');
+}, 5000);
 // logger.info('Hello World!');
-logger.error('Fatal')
+logger.error('Fatal');
 
-logger.info(`Solution: ${quadratic.solve(1, 4, 4)}`)
-logger.info(`Solution: ${quadratic.solve(1, 4, -4)}`)
+logger.info(`Solution: ${quadratic.solve(1, 4, 4)}`);
+logger.info(`Solution: ${quadratic.solve(1, 4, -4)}`);
 
 // quadratic.solve_promise(1,4,4)
 //     .then((solution)=> logger.info(solution))
@@ -31,8 +31,8 @@ logger.info(`Solution: ${quadratic.solve(1, 4, -4)}`)
 
 quadratic.solve_async(1, 4, 4)
   .then((solution) => logger.info(solution))
-  .catch((error) => logger.error(error))
+  .catch((error) => logger.error(error));
 
 quadratic.solve_async(1, 4, 40)
   .then((solution) => logger.info(solution))
-  .catch((error) => logger.error(error.message))
+  .catch((error) => logger.error(error.message));
