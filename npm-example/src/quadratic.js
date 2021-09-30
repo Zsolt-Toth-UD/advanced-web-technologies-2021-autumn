f2485d8d27825104fed02a5419c460bcf2dbacd6
Zsolt Toth <tothzs87@gmail.com>
2021-09-30 10:28:36 +0200

    refactor(npm-example): refactor quadratic.js according to eslint

--- npm-example/src/quadratic.js
+++ npm-example/src/quadratic.js
@@ -1,20 +1,20 @@
 
 const solve = (a, b, c) => {
   if (b * b - 4 * a * c < 0.0) return
-  if (b * b - 4 * a * c == 0.0) return (-1 * b) / (2 * a)
+  if (b * b - 4 * a * c === 0.0) return (-1 * b) / (2 * a)
   return [
     (-1 * b + Math.sqrt(b * b - 4 * a * c)) / (2 * a),
     (-1 * b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)
   ]
 }
 
-const solve_promise = (a, b, c) => {
+const solvePromise = (a, b, c) => {
   return new Promise((resolve, reject) => {
     if (b * b - 4 * a * c < 0.0) {
-      reject(`Equation: ${a}x^2 + ${b}x + ${c} has no solution!`)
+      reject(new Error(`Equation: ${a}x^2 + ${b}x + ${c} has no solution!`))
       return
     }
-    if (b * b - 4 * a * c == 0.0) {
+    if (b * b - 4 * a * c === 0.0) {
       resolve((-1 * b) / (2 * a))
       return
     }
@@ -26,11 +26,11 @@ const solve_promise = (a, b, c) => {
   })
 }
 
-const solve_async = async (a, b, c) => {
+const solveAsync = async (a, b, c) => {
   if (b * b - 4 * a * c < 0.0) {
     throw Error(`Equation: ${a}x^2 + ${b}x + ${c} has no solution!`)
   }
-  if (b * b - 4 * a * c == 0.0) {
+  if (b * b - 4 * a * c === 0.0) {
     return (-1 * b) / (2 * a)
   }
   return [
@@ -40,6 +40,6 @@ const solve_async = async (a, b, c) => {
 }
 module.exports = {
   solve: solve,
-  solve_promise: solve_promise,
-  solve_async: solve_async
+  solve_promise: solvePromise,
+  solve_async: solveAsync
 }
