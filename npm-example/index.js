eab5716b480d26bff5992c48e1973ff68c01f460
Zsolt Toth <tothzs87@gmail.com>
2021-09-30 10:17:40 +0200

    feat(npm-example): add eslint default config

--- npm-example/index.js
+++ npm-example/index.js
@@ -1,25 +1,25 @@
-const winston = require('winston');
-const quadratic = require("./src/quadratic");
+const winston = require('winston')
+const quadratic = require('./src/quadratic')
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: {service: 'user-service'},
-    transports: [
-        new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
-        new winston.transports.File({filename: 'logs/combined.log'}),
-        new winston.transports.Console()
-    ],
-});
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.Console()
+  ]
+})
 
 // console.log('Hello World!');
-setTimeout(()=>{
-    logger.info('Hello World!');
-}, 5000);
+setTimeout(() => {
+  logger.info('Hello World!')
+}, 5000)
 // logger.info('Hello World!');
-logger.error('Fatal');
+logger.error('Fatal')
 
-logger.info(`Solution: ${quadratic.solve(1, 4, 4)}`);
-logger.info(`Solution: ${quadratic.solve(1, 4, -4)}`);
+logger.info(`Solution: ${quadratic.solve(1, 4, 4)}`)
+logger.info(`Solution: ${quadratic.solve(1, 4, -4)}`)
 
 // quadratic.solve_promise(1,4,4)
 //     .then((solution)=> logger.info(solution))
@@ -29,10 +29,10 @@ logger.info(`Solution: ${quadratic.solve(1, 4, -4)}`);
 //     .then((solution)=> logger.info(solution))
 //     .catch((error)=>logger.error(error));
 
-quadratic.solve_async(1,4,4)
-    .then((solution)=> logger.info(solution))
-    .catch((error)=>logger.error(error));
+quadratic.solve_async(1, 4, 4)
+  .then((solution) => logger.info(solution))
+  .catch((error) => logger.error(error))
 
-quadratic.solve_async(1,4,40)
-    .then((solution)=> logger.info(solution))
-    .catch((error)=>logger.error(error.message));
+quadratic.solve_async(1, 4, 40)
+  .then((solution) => logger.info(solution))
+  .catch((error) => logger.error(error.message))
--- npm-example/src/quadratic.js
+++ npm-example/src/quadratic.js
@@ -1,45 +1,45 @@
 
-const solve = (a,b,c) => {
-    if( b*b - 4*a*c < 0.0) return;
-    if( b*b - 4*a*c == 0.0) return (-1*b) / (2*a);
-    return [
-        (-1*b + Math.sqrt(b*b - 4*a*c) ) / (2*a),
-        (-1*b - Math.sqrt(b*b - 4*a*c) ) / (2*a)
-    ]
+const solve = (a, b, c) => {
+  if (b * b - 4 * a * c < 0.0) return
+  if (b * b - 4 * a * c == 0.0) return (-1 * b) / (2 * a)
+  return [
+    (-1 * b + Math.sqrt(b * b - 4 * a * c)) / (2 * a),
+    (-1 * b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)
+  ]
 }
 
-const solve_promise = (a,b,c) => {
-    return new Promise(((resolve, reject) => {
-        if( b*b - 4*a*c < 0.0){
-            reject(`Equation: ${a}x^2 + ${b}x + ${c} has no solution!`);
-            return;
-        }
-        if( b*b - 4*a*c == 0.0){
-            resolve((-1*b) / (2*a));
-            return;
-        }
+const solve_promise = (a, b, c) => {
+  return new Promise((resolve, reject) => {
+    if (b * b - 4 * a * c < 0.0) {
+      reject(`Equation: ${a}x^2 + ${b}x + ${c} has no solution!`)
+      return
+    }
+    if (b * b - 4 * a * c == 0.0) {
+      resolve((-1 * b) / (2 * a))
+      return
+    }
 
-       resolve([
-            (-1*b + Math.sqrt(b*b - 4*a*c) ) / (2*a),
-            (-1*b - Math.sqrt(b*b - 4*a*c) ) / (2*a)
-        ]);
-    }));
+    resolve([
+      (-1 * b + Math.sqrt(b * b - 4 * a * c)) / (2 * a),
+      (-1 * b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)
+    ])
+  })
 }
 
-const solve_async = async (a,b,c) => {
-        if( b*b - 4*a*c < 0.0){
-            throw Error(`Equation: ${a}x^2 + ${b}x + ${c} has no solution!`);
-        }
-        if( b*b - 4*a*c == 0.0){
-            return (-1*b) / (2*a);
-        }
-        return [
-            (-1*b + Math.sqrt(b*b - 4*a*c) ) / (2*a),
-            (-1*b - Math.sqrt(b*b - 4*a*c) ) / (2*a)
-        ];
+const solve_async = async (a, b, c) => {
+  if (b * b - 4 * a * c < 0.0) {
+    throw Error(`Equation: ${a}x^2 + ${b}x + ${c} has no solution!`)
+  }
+  if (b * b - 4 * a * c == 0.0) {
+    return (-1 * b) / (2 * a)
+  }
+  return [
+    (-1 * b + Math.sqrt(b * b - 4 * a * c)) / (2 * a),
+    (-1 * b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)
+  ]
 }
 module.exports = {
-    solve : solve,
-    solve_promise: solve_promise,
-    solve_async: solve_async
+  solve: solve,
+  solve_promise: solve_promise,
+  solve_async: solve_async
 }
