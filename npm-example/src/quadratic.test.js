a1cbf96448a9f9c5c41e0b4f6b60e84e3fa85510
Zsolt Toth <tothzs87@gmail.com>
2021-10-07 10:06:50 +0200

    test(npm-example): add quadratic.test.js

--- npm-example/src/quadratic.test.js
+++ npm-example/src/quadratic.test.js
@@ -1,5 +1,19 @@
+const quadratic = require('./quadratic');
+
 describe('Quadratic Solver', () => {
-  it('dummy test', () => {
+  it('tests if true is true', () => {
     expect(true).toBe(true);
+  });
+
+  it('solves an equation without solution', () => {
+    // Given
+    const a = 1;
+    const b = 0;
+    const c = 1;
+    const expected = undefined;
+    // When
+    const actual = quadratic.solve(a, b, c);
+    // Then
+    expect(actual).toBe(expected);
   });
 });
