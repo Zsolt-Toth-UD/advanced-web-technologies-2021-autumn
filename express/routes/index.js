670f9c5517df1c61790f9517bcbec9a0f002cda0
Zsolt Toth <tothzs87@gmail.com>
2021-10-14 11:16:12 +0200

    feat(express): add query, body and path variable examples

--- express/routes/index.js
+++ express/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello', (req,res,nxt)=>{
+  name = req.query['name'] || 'World';
+  res.status(200).send(`Hello ${name}!`);
+});
+
+router.get('/hello/:name', (req,res,nxt)=> {
+  res.status(200).send(`Hello ${req.params.name}!`);
+});
+
+router.post('/hello', (req,res,next)=>{
+  console.log({body: req.body});
+  name = req.body['name'] || 'World';
+  res.status(200).send(`Hello ${name}!`);
+});
+
 module.exports = router;
