475ef191746f7bc84efa6d799e43973724cc9196
Zsolt Toth <tothzs87@gmail.com>
2021-10-21 10:37:48 +0200

    feat(express): add swagger config and documentations

--- express/app.js
+++ express/app.js
@@ -17,4 +17,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'MERN Template Express API with Swagger',
+            version: '0.1.0',
+            description:
+                'Lorem ipsum ...',
+            license: {
+                name: 'MIT',
+                url: 'https://spdx.org/licenses/MIT.html'
+            },
+            contact: {
+                name: 'Zsolt Toth',
+                url: 'https://github.com/ZsoltToth',
+                email: 'toth.zsolt@uni-eszterhazy.hu'
+            }
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/'
+            }
+        ]
+    },
+    apis: [path.join(__dirname, '/routes/*.js')]
+};
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
+
+
 module.exports = app;
--- express/routes/index.js
+++ express/routes/index.js
@@ -7,12 +7,52 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/**
+ * @swagger
+ * /hello:
+ *  get:
+ *    summary: Hello
+ *    responses:
+ *      '200':
+ *        description: OK
+ */
 router.get('/hello', greetingsController.getHello);
 
+/**
+ * @swagger
+ * /hello/{name}:
+ *  get:
+ *    summary: Hello With Path Variable
+ *    parameters:
+ *      - name: name
+ *        in: path
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: OK
+ */
 router.get('/hello/:name', (req,res,nxt)=> {
   res.status(200).send(`Hello ${req.params.name}!`);
 });
 
+/**
+ * @swagger
+ * /hello:
+ *  post:
+ *    summary: Hello with POST Request
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *    responses:
+ *      '200':
+ *        description: OK
+ */
 router.post('/hello', (req,res,next)=>{
   console.log({body: req.body});
   name = req.body['name'] || 'World';
